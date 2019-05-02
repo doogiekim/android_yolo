1d1193c499613f8bce15512cfc5406e798a9d453
Doowook Kim <doogiekim@gmail.com>
2019-05-03 08:58:35 +0900

    Update image buffer

--- app/src/main/java/com/samsung/android/app/yolo/ImageBuffer.java
+++ app/src/main/java/com/samsung/android/app/yolo/ImageBuffer.java
@@ -1,8 +1,6 @@
 package com.samsung.android.app.yolo;
 
 import android.graphics.Bitmap;
-import android.os.SystemClock;
-import android.util.Log;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -12,7 +10,6 @@ class ImageBuffer {
     private static final String TAG = "ImageBuffer";
 
     private ByteBuffer mByteBuffer;
-    private int[] intValues = new int[YoloConstants.IMAGE_WIDTH * YoloConstants.IMAGE_HEIGHT];
 
     public ImageBuffer() {
         initBuffer();
@@ -33,23 +30,19 @@ class ImageBuffer {
             return;
         }
         mByteBuffer.rewind();
-        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-        // Convert the image to floating point.
-        int pixel = 0;
-        long startTime = SystemClock.uptimeMillis();
+        
+        int[] pixels = new int[YoloConstants.IMAGE_WIDTH * YoloConstants.IMAGE_HEIGHT];
+        bitmap.getPixels(pixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        int n = 0;
         for (int i = 0; i < YoloConstants.IMAGE_WIDTH; ++i) {
             for (int j = 0; j < YoloConstants.IMAGE_HEIGHT; ++j) {
-                final int val = intValues[pixel++];
-                addPixelValue(val);
+                final int pixel = pixels[n++];
+                mByteBuffer.putFloat(((pixel >> 16) & 0xFF) / 255.f);
+                mByteBuffer.putFloat(((pixel >> 8) & 0xFF) / 255.f);
+                mByteBuffer.putFloat((pixel & 0xFF) / 255.f);
             }
         }
-        long endTime = SystemClock.uptimeMillis();
-        Log.d(TAG, "Timecost to put values into ByteBuffer: " + (endTime - startTime));
     }
 
-    private void addPixelValue(int pixelValue) {
-        mByteBuffer.putFloat(((pixelValue >> 16) & 0xFF) / 255.f);
-        mByteBuffer.putFloat(((pixelValue >> 8) & 0xFF) / 255.f);
-        mByteBuffer.putFloat((pixelValue & 0xFF) / 255.f);
-    }
 }
